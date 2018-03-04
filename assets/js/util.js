31dd2e18a32a38ee96d5f435ae251214f5996856
kjuhee0712 <kjuhee0712@gmail.com>
2018-03-04 17:35:21 +0900

    no message

--- assets/js/util.js
+++ assets/js/util.js
@@ -177,7 +177,7 @@
 
         }
 
-        $("body").css({overflow:'hidden'}).bind('touchmove', function(e){e.preventDefault()});
+       
 
         // Event: Touch stuff.
         $this.on('touchstart', function(event) {
